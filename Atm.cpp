7ae211fb55844b91392a9f6c097c7b3d4be6a2fa
nima makhmali <154757931+nimamakhmali@users.noreply.github.com>
2024-03-08 13:58:29 +0330

    functions

--- Atm.cpp
+++ Atm.cpp
@@ -50,7 +50,7 @@ void write_account();
 void display_sp();
 void modify_account();
 void delete_account();
-void display_all();
+void display_all(int);
 void deposit_withdraw();
 void Titr();
 
@@ -61,4 +61,40 @@ int main()
   cout<<endl;
  cout << "                        Nima Bank"<<"\n\n";
  return 0;
+}
+
+void menu()
+{
+  int num , ch ;
+  do
+  {
+    		cout<<endl;
+		cout<<"\n\n\n\t   MAIN MENU                                                                                     " ;
+		cout<<"\n\n\t   01. NEW ACCOUNT                     --              EJAD HESAB .01                              " ;
+		cout<<"\n\n\t   02. DEPOSIT AMOUNT                  --              SEPORDE(AFZAYESH) VAJH .02                  " ;
+		cout<<"\n\n\t   03. WITHDRAW AMOUNT                 --              BARDASHT(KAHESH) VAJH .03                   " ;
+		cout<<"\n\n\t   04. BALANCE ENQUIRY                 --              ESTELAM MOJODI .04                          " ;
+		cout<<"\n\n\t   05. ALL ACCOUNT HOLDER LIST         --              FEHREST HESAB HA .05                        " ; 
+		cout<<"\n\n\t   06. CLOSE AN ACCOUNT                --              BASTAN HESAB .06                            " ;
+		cout<<"\n\n\t   07. MODIFY AN ACCOUNT               --              ESLAH HESAB .07                             " ;
+		cout<<"\n\n\t   08. EXIT                            --              Khoroj .08                                  " ;
+		cout<<"\n\n\n\t Select Your Option (1-8) ";
+		cin>>ch;
+		cout<<endl;
+       
+
+       switch (ch)
+       {
+       case 1:
+           write_account();       
+        break;
+       case 2:
+         cout<<"\n\n\tEnter The account No. : "; cin>>num;
+         deposit_withdraw(num , 1);
+         break;
+       default:
+        break;
+       }
+
+  }while(ch!=8);
 }
