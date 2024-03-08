385644253f065181ab8079a1283b03b55abb3ae4
nima makhmali <154757931+nimamakhmali@users.noreply.github.com>
2024-03-08 13:45:40 +0330

    add class

---
+++ Atm.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+class account 
+{
+ private:
+ int acno;
+ char name[50];
+ int deposit;
+ char type;
+ public:
+ void create_account(int acno)
+ {
+
+ }
+ void show_account()
+ {
+
+ }
+ void modify()
+ {
+
+ }
+ void dep()
+ {
+
+ }
+ void draw()
+ {
+
+ }
+ void report()
+ {
+
+ }
+ int retacno()
+ {
+
+ }
+ int retdeposit()
+ {
+
+ }
+ char rettype()
+ {
+
+ }
+};
